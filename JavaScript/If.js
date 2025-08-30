f28d968d1e572790cf1278190f78b5ee6a4c5c08
Mota-Silva <carlos.michael2002@gmail.com>
2025-08-29 21:05:33 -0300

    aula dia 29082025

---
+++ JavaScript/If.js
@@ -0,0 +1,8 @@
+let idade = Math.floor(Math.random() * 100) + 1; // Gera uma idade aleatória entre 1 e 100
+
+if (idade >= 18) {
+    console.log('Maior de idade!' + idade + ' Anos');
+} 
+else {
+    console.log('Menor de idade!' + idade);
+}
